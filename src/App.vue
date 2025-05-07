00b5d3289ec0a9f6904d3c95d80e5f6a8b65b38d
mustafa <mustafa@trison.uk>
2025-05-07 15:29:26 +0100

    home screen layout done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <HomeScreen/>
-  <div class="logo">
-    <img src="@/assets/main-burger.png" alt="Restaurant Logo" />
-  </div>
-  <div class="cta">
-    <p class="text-primary">Touch to start</p>
-    <SvgIcon tag="touch" css="icon touch" />
-  </div>
-  
 
   <IconSprite />
   
--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="header-container">
-    <SvgIcon tag="logo" css="icon logo" />
-    <h1>Restaurant</h1>
+  <div class="home-screen">
+    <div class="logo-header">
+      <SvgIcon tag="logo" css="icon logo" />
+      <h1>Orderly</h1>
+    </div>
+    <div class="main-meal">
+      <img src="@/assets/main-burger.png" alt="Restaurant Logo" />
+    </div>
+    <div class="cta">
+      <SvgIcon tag="touch" css="icon touch" />
+      <h1 class="text-primary">Touch to start</h1>
+    </div>
   </div>
 </template>
-
 <script setup>
-import { SvgIcon} from '@/components'
+import { SvgIcon } from '@/components'
 </script>
